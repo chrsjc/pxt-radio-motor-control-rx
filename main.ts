c44ca5c62d256761eae7eeba69f6611e59085621
chrsjc <43938510+chrsjc@users.noreply.github.com>
2019-12-22 19:40:37 +0000

    Original fixed point implementation

--- main.ts
+++ main.ts
@@ -1,7 +1,71 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . # . .
-    # . . . #
-    . # # # .
-    `);
+radio.onReceivedValue(function (name, value) {
+    packetReceived = true
+    if (name == "speedL") {
+        speedL = value
+        updateMotor(kitronik.Motors.Motor2, speedL)
+    } else if (name == "speedR") {
+        speedR = value
+        updateMotor(kitronik.Motors.Motor1, speedR)
+    }
+})
+function plotMotor(x: number, y: number) {
+    for (let i = 0; i <= 5 - 1; i++) {
+        if (i == y) {
+            led.plot(x, i)
+        } else {
+            led.unplot(x, i)
+        }
+    }
+}
+function plotMotorL(speedL: number) {
+    let yL = speedToScreen(speedL)
+    plotMotor(0, yL)
+}
+function plotMotorR(speedR: number) {
+    let yR = speedToScreen(speedR)
+    plotMotor(4, yR)
+}
+function plotPacketReceived() {
+    if (packetReceived) {
+        led.toggle(2, 2)
+        packetReceived = false
+    } else {
+        led.plot(2, 2)
+    }
+}
+let packetReceived = false
+let speedR = 0
+let speedL = 0
+let name = ""
+let value = 0
+function speedToScreen(speed: number) {
+
+    let y = pins.map(
+        speed,
+        -100,
+        100,
+        4,
+        0
+    )
+
+    return y
+}
+function updateMotor(motor: kitronik.Motors, speed: number) {
+
+    if (speed > 0) {
+        kitronik.motorOn(motor, kitronik.MotorDirection.Forward, speed)
+    } else if (speed < 0) {
+        kitronik.motorOn(motor, kitronik.MotorDirection.Reverse, Math.abs(speed))
+    } else {
+        kitronik.motorOff(motor)
+    }
+}
+radio.setGroup(1)
+control.inBackground(function () {
+    while (true) {
+        plotMotorL(speedL)
+        plotMotorR(speedR)
+        plotPacketReceived()
+        basic.pause(100)
+    }
+})
