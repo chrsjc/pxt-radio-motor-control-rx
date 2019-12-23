410f7387f3449896e71df00717c26481cc37433f
chrsjc <43938510+chrsjc@users.noreply.github.com>
2019-12-23 16:03:19 +0000

    Extract HeartBeatLed class

--- main.ts
+++ main.ts
@@ -64,15 +64,40 @@ class VerticalBar {
     }
 }
 
+class HeartBeatLed {
+    private readonly x: number;
+    private readonly y: number;
+    private _toggle: boolean = false;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plot() {
+        if (this._toggle) {
+            led.toggle(this.x, this.y)
+            this._toggle = false
+        } else {
+            led.plot(this.x, this.y)
+        }
+    }
+
+    toggle() {
+        this._toggle = true;
+    }
+}
+
 enum Side {
     Left = 0, Right = 1
 }
 
 const motor = [new Motor(kitronik_motor_driver.Motors.Motor2), new Motor(kitronik_motor_driver.Motors.Motor1)]
 const verticalBar = [new VerticalBar(0, motorMin, motorMax), new VerticalBar(4, motorMin, motorMax)];
+const heartBeatLed = new HeartBeatLed(2, 2);
 
 radio.onReceivedValue(function (name, value) {
-    packetReceived = true
+    heartBeatLed.toggle();
     if (name == "speedL") {
         motor[Side.Left].speed = value
     } else if (name == "speedR") {
@@ -80,16 +105,6 @@ radio.onReceivedValue(function (name, value) {
     }
 })
 
-function plotPacketReceived() {
-    if (packetReceived) {
-        led.toggle(2, 2)
-        packetReceived = false
-    } else {
-        led.plot(2, 2)
-    }
-}
-
-let packetReceived = false
 let name = ""
 let value = 0
 
@@ -99,7 +114,7 @@ control.inBackground(function () {
     while (true) {
         verticalBar[Side.Left].plot(motor[Side.Left].speed)
         verticalBar[Side.Right].plot(motor[Side.Right].speed)
-        plotPacketReceived()
+        heartBeatLed.plot()
         basic.pause(100)
     }
 })
